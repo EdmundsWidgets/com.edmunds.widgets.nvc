e8b38d6b96ec8ecff5baf2a12465c8de58ad5d43
Dzmitry Yurkavets <dzmitry_yurkavets@epam.com>
2014-02-24 18:26:00 +0300

    added test examples

---
+++ test/unit/basic-test.spec.js
@@ -0,0 +1,11 @@
+describe('basic test suite', function() {
+
+    it('should be truthy', function() {
+        expect(true).toBeTruthy();
+    });
+
+    it('should be falsy', function() {
+        expect(false).toBeFalsy();
+    });
+
+});
---
+++ test/unit/requirejs-test.spec.js
@@ -0,0 +1,15 @@
+define(function() {
+
+    describe('requirejs test suite', function() {
+
+        it('should be truthy', function() {
+            expect(true).toBeTruthy();
+        });
+
+        it('should be falsy', function() {
+            expect(false).toBeFalsy();
+        });
+
+    });
+
+});
