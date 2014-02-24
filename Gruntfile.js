7beae0e46cea4f025706a604fd47b2418e116075
Dzmitry Yurkavets <dzmitry_yurkavets@epam.com>
2014-02-24 19:21:03 +0300

    configured clean and copy tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,24 @@ module.exports = function(grunt) {
 
         pkg: grunt.file.readJSON('package.json'),
 
+        dir: {
+            build: 'dist/'
+        },
+
+        // https://github.com/gruntjs/grunt-contrib-clean
+        clean: {
+            build: ['<%= dir.build %>']
+        },
+
+        // https://github.com/gruntjs/grunt-contrib-copy
+        copy: {
+            build: {
+                files: [
+                    { expand: true, src: ['img/**', 'less/**/*.less'], dest: '<%= dir.build %>/' }
+                ]
+            }
+        },
+
         // https://github.com/karma-runner/grunt-karma
         karma: {
             unit: {
@@ -16,11 +34,18 @@ module.exports = function(grunt) {
     });
 
     // plugins
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-karma');
 
     // tasks
     grunt.registerTask('test', [
         'karma:unit'
+    ]);
+
+    grunt.registerTask('build', [
+        'clean:build',
+        'copy:build'
     ]);
 
 };
