da9ee97bcebbcb0b690d5e374ddc05f61710a7d6
Dzmitry Yurkavets <dyurkavets@gmail.com>
2014-04-01 18:45:51 +0300

    added example module with example test

---
+++ src/example.js
@@ -0,0 +1,7 @@
+define({
+
+    foo: function() {
+        return 'bar';
+    }
+
+});
---
+++ test/unit/example.spec.js
@@ -0,0 +1,11 @@
+define(['example'], function(example) {
+
+    describe('test for example', function() {
+
+        it('should return bar', function() {
+            expect(example.foo()).toBe('bar');
+        });
+
+    });
+
+});
